59102241c41694f3b473ea4624aea0b1a4cbe141
Brett Hardin <hardin.brett@gmail.com>
2012-01-16 17:06:50 -0800

    Adding Gravatar and Email

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,9 @@ module ApplicationHelper
    "#{title} | Potential Employee Application"
   end
 
+  def avatar_url(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    "http://gravatar.com/avatar/#{gravatar_id}.png"
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120117003709) do
+ActiveRecord::Schema.define(:version => 20120117005355) do
 
   create_table "candidates", :force => true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20120117003709) do
     t.string   "github_name"
     t.string   "linked_in"
     t.text     "notes"
+    t.string   "email"
   end
 
 end
